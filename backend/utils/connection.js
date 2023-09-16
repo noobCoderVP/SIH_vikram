89b9aa34c656fcc8423b43d555805deed4c4462c
ProCoderVP <vaibhavpatel02892@gmail.com>
2023-09-16 19:17:11 +0530

    Connection with database

---
+++ backend/utils/connection.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+export async function connect() {
+    await mongoose.connect(
+        `mongodb://vaibhav:${process.env.DB_PASS}@ac-p1f2jq8-shard-00-00.nm9w35r.mongodb.net:27017,ac-p1f2jq8-shard-00-01.nm9w35r.mongodb.net:27017,ac-p1f2jq8-shard-00-02.nm9w35r.mongodb.net:27017/nyaybazaar?ssl=true&replicaSet=atlas-di7g4w-shard-0&authSource=admin&retryWrites=true&w=majority`,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        },
+    );
+}
+
+export function disconnect() {
+    mongoose.disconnect();
+}
