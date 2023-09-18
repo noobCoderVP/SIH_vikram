4e870b48eb850223536826f96783efe36ca59849
toshan-luktuke <toshan1603@gmail.com>
2023-09-18 13:10:43 +0530

    Remove unecessary components

--- frontend/components/Title.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-const Title = styled.h1`
-  font-size: 1.5em;
-`;
-
-export default Title;
--- frontend/components/WhiteBox.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const WhiteBox = styled.div`
-  background-color: #fff;
-  border-radius: 10px;
-  padding: 30px;
-`;
-
-export default WhiteBox;
