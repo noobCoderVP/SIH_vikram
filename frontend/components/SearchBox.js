d8b9c6b9570356ab24c3b322e126a9eef1317d42
Sarrah Bastawala <sarrah.basta@gmail.com>
2023-09-19 11:21:33 +0530

    fixed issues caused by merge

--- frontend/components/SearchBox.js
+++ frontend/components/SearchBox.js
@@ -28,11 +28,11 @@ const SearchBox = () => {
                             dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 placeholder="Describe your problem and our AI will take care of the rest !" 
                                 rows="4" 
-                                required 
-                                value={inputText} 
-                                onChange={handleInputChange} />
+                                required />
+                                {/* value={inputText} 
+                                onChange={handleInputChange} /> */}
 
-                            <button onClick={handleClick} type="submit" class="right-2.5 text-white 
+                            <button type="submit" class="right-2.5 text-white 
                             bg-slate-700 hover:bg-slate-800 focus:ring-4 focus:outline-none focus:ring-blue-300 
                             font-medium rounded-lg text-sm px-4 py-2 ml-2 mb-auto mt-auto dark:bg-slate-600 dark:hover:bg-blue-700 
                             dark:focus:ring-blue-800">Search</button>
