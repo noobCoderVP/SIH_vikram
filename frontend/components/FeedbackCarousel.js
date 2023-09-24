335db5abc190060e77cc079148abfa5333382138
toshan-luktuke <toshan1603@gmail.com>
2023-09-24 20:14:20 +0530

    Update Clear Button colour and Feedback Background

--- frontend/components/FeedbackCarousel.js
+++ frontend/components/FeedbackCarousel.js
@@ -11,27 +11,30 @@ const FeedbackCarousel = () => {
         imageUrl: "", // Provide the URL to the person's image
     };
     return (
-        <section className="bg-white">
+        <section className="bg-white pb-4">
             <div className="w-full mx-auto text-center py-6 bg-slate-100">
                 <h1 className="text-xl md:text-2xl lg:text-3xl font-semibold text-black mb-4">
                     Our Rave Reviews
                 </h1>
             </div>
             <div className="flex w-full flex-row justify-evenly gap-4 flex-wrap">
+
                 <div>
                     <FeedbackCard {...feedbackData}></FeedbackCard>
                 </div>
                 <div>
                     <FeedbackCard {...feedbackData}></FeedbackCard>
                 </div>
-                <div className="flex justify-center items-center">
+                <div className="">
                     <FeedbackCard {...feedbackData}></FeedbackCard>
                 </div>
 
-                <div className="flex justify-center items-center">
+                <div className="">
                     <FeedbackCard {...feedbackData}></FeedbackCard>
-                </div>
+                </div>                
+
             </div>
+
         </section>
     );
 };
